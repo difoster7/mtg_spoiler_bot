a93929986a3823cb41c66e6e8ee8b5497707baaf
difoster7 <95183391+difoster7@users.noreply.github.com>
2022-06-29 19:25:17 -0400

    started bot testing

---
+++ bot_test.py
@@ -0,0 +1,23 @@
+# Testing bot stuff
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+with open('token.txt') as f:
+    _ID = f.readline()
+
+client.run(_ID)
